49f748f572c761b64cb734b0784bda0631a53301
David Pink <dvdpink@gmail.com>
2019-10-24 18:43:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,8 +16,6 @@ function logWhisper(string){
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase()===string) {return "I can\'t hear you!"}
-}
-
-funtion sayHiToGrandma (string) {
+};
 if (string.toUpperCase()===string) {return "YES INDEED!"}
 }
